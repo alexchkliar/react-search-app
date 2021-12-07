4310e4cb5b0a69ec18861879aeb9f78f794dbb2b
Alexander Chkliar <alex.chkliar@gmail.com>
2021-12-07 00:00:24 -0500

    completed besides selecting gif functionality

---
+++ src/components/app.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+import giphy from 'giphy-api';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "YzNkhYFAUgVK6UuK6Z"
+    };
+
+    // this.search("homer thinking");
+  }
+
+  search = (query) => {
+    giphy('Rnr92kbYxwUVFdvhceQzDgV9GdEfIEqI').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
